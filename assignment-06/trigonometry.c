e9bb73b4593ad0e520685c63be1b8d3af716790d
Precioux <79986497+Precioux@users.noreply.github.com>
2021-04-28 02:05:52 +0430

    Update trigonometry.c

--- assignment-06/trigonometry.c
+++ assignment-06/trigonometry.c
@@ -1,13 +1,28 @@
 /*
 * Project name: trigonometry
 --------------------------------------------
-    ID:
-    Name:
-    Mail:
+    ID:9839039
+    Name:Samin Mahdipour
+    Mail:Uni.mahdipour@gmail.com
 --------------------------------------------
 */
 #include <stdio.h>
 
 int main(){
-    //write your code here
+       double s,sign_c,c,teta;
+    puts("Enter Sinus:");
+    scanf( "%lf",&s);
+    puts("Enter Sign Cosinus:  for - enter -1 for + enter +1 ");
+    scanf("%lf",&sign_c);
+    c=sqrt(1-s*s);
+    c=sign_c*c;
+    if(c>0 && s>0)
+        printf("Zone 1");
+    if(c<0 && s>0)
+        printf("Zone 2");
+    if(c<0 && s<0)
+        printf("Zone 3");
+    if(c>0 && s<0)
+        printf("Zone 4");
+    return 0;
 }
